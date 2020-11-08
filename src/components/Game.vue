1acda61e35bd8a6755ad01fd60b9d7272e658fb2
tomriddle7 <tomriddle7@naver.com>
2020-11-08 16:23:31 +0900

    Create Game component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-    <h1>Game</h1>
-  </div>
+<div>
+  <Products v-bind:appData="topFreeGames">
+  </Products>
+  <Products v-bind:appData="topPaidGames">
+  </Products>
+</div>
 </template>
-
 <script>
+import Products from "./Products";
+
 export default {
-  name : 'game'
+  name : "application",
+  components: { Products },
+  created: function() {
+    this.$axios.get("https://cors-anywhere.herokuapp.com/https://rss.itunes.apple.com/api/v1/kr/ios-apps/top-free/games/25/explicit.json")
+      .then((response) => {
+        this.topFreeGames = response.data.feed;
+        // console.log(this.topFreeApps);
+      });
+    this.$axios.get("https://cors-anywhere.herokuapp.com/https://rss.itunes.apple.com/api/v1/kr/ios-apps/top-paid/games/25/explicit.json")
+      .then((response) => {
+        this.topPaidGames = response.data.feed;
+        // console.log(this.topPaidApps);
+      });
+  },
+  data() {
+    return {
+      topFreeGames: null,
+      topPaidGames: null
+    }
+  }
 }
 </script>
