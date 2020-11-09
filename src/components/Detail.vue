1a4a01ca88c8a6dbab57263b5209f004469bb4f1
tomriddle7 <tomriddle7@naver.com>
2020-11-09 10:03:29 +0900

    Update formating

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,72 +1,97 @@
 <template>
-<div class="Container" v-if="productDetail">
-  <button class="BackButton" v-on:click="goBack()">◀홈화면</button>
-  <div class="Summary" v-if="productDetail">
-    <div class="Scope20">
-      <img class="Icon" :src="productDetail.artworkUrl100" />
+  <div class="Container" v-if="productDetail">
+    <button class="BackButton" v-on:click="goBack()">◀홈화면</button>
+    <div class="Summary" v-if="productDetail">
+      <div class="Scope20">
+        <img class="Icon" :src="productDetail.artworkUrl100" />
+      </div>
+      <div class="Scope50">
+        <h1 class="TitleName">{{ productDetail.trackName }}</h1>
+        <h1 class="TitleName">{{ productDetail.artistName }}</h1>
+      </div>
+      <div class="Scope30">
+        <button
+          class="StoreButton"
+          v-on:click="iTune(productDetail.trackViewUrl)"
+        >
+          {{ productDetail.formattedPrice }}
+        </button>
+        <h1 class="TitleName">{{ productDetail.trackContentRating }}</h1>
+      </div>
     </div>
-    <div class="Scope50">
-      <h1 class="TitleName">{{productDetail.trackName}}</h1>
-      <h1 class="TitleName">{{productDetail.artistName}}</h1>
+    <div class="ScreenShotP">
+      <img
+        class="ScreenShot"
+        :src="val"
+        v-for="(val, key) in productDetail.screenshotUrls"
+        v-bind:key="key"
+      />
     </div>
-    <div class="Scope30">
-      <button class="StoreButton" v-on:click="iTune(productDetail.trackViewUrl)">{{productDetail.formattedPrice}}</button>
-      <h1 class="TitleName">{{productDetail.trackContentRating}}</h1>
+    <div class="Explanation">
+      <span>{{ productDetail.description }}</span>
+    </div>
+    <div class="UpdateviewP">
+      <div class="Title">새로운 기능</div>
+      <div class="UpdateDate">
+        <div class="ViewLeft width50">버전 {{ productDetail.version }}</div>
+        <div class="ViewRight width50">
+          {{ productDetail.currentVersionReleaseDate.substr(0, 4) }}년
+          {{ productDetail.currentVersionReleaseDate.substr(5, 2) }}월
+          {{ productDetail.currentVersionReleaseDate.substr(8, 2) }}일
+        </div>
+      </div>
+      <div class="UpdateDetail">
+        <span>{{ productDetail.releaseNotes }}</span>
+      </div>
+    </div>
+    <div class="OverviewP">
+      <div class="Title">정보</div>
+      <div class="Overview">
+        <div class="ViewLeft width25">제공자</div>
+        <div class="ViewRight width75">{{ productDetail.sellerName }}</div>
+      </div>
+      <div class="Overview">
+        <div class="ViewLeft width25">크기</div>
+        <div class="ViewRight width75">
+          {{ (productDetail.fileSizeBytes / 1024 / 1024).toFixed(1) }}MB
+        </div>
+      </div>
+      <div class="Overview">
+        <div class="ViewLeft width25">카테고리</div>
+        <div class="ViewRight width75">
+          {{ productDetail.primaryGenreName }}
+        </div>
+      </div>
+      <div class="Overview">
+        <div class="ViewLeft width25">언어</div>
+        <div class="ViewRight width75">
+          {{ productDetail.languageCodesISO2A.join(" | ") }}
+        </div>
+      </div>
+      <div class="Overview">
+        <div class="ViewLeft width25">연령 등급</div>
+        <div class="ViewRight width75">
+          {{ productDetail.contentAdvisoryRating }}
+        </div>
+      </div>
     </div>
   </div>
-  <div class="ScreenShotP">
-    <img class="ScreenShot" :src="val" v-for="(val, key) in productDetail.screenshotUrls" v-bind:key="key"/>
-  </div>
-  <div class="Explanation">
-    <span>{{productDetail.description}}</span>
-  </div>
-  <div class="UpdateviewP">
-    <div class="Title">새로운 기능</div>
-    <div class="UpdateDate">
-      <div class="ViewLeft width50">버전 {{productDetail.version}}</div>
-      <div class="ViewRight width50">{{productDetail.currentVersionReleaseDate.substr(0,4)}}년 {{productDetail.currentVersionReleaseDate.substr(5,2)}}월 {{productDetail.currentVersionReleaseDate.substr(8,2)}}일</div>
-    </div>
-    <div class="UpdateDetail">
-      <span>{{productDetail.releaseNotes}}</span>
-    </div>
-  </div>
-  <div class="OverviewP">
-    <div class="Title">정보</div>
-    <div class="Overview">
-      <div class="ViewLeft width25">제공자</div>
-      <div class="ViewRight width75">{{productDetail.sellerName}}</div>
-    </div>
-    <div class="Overview">
-      <div class="ViewLeft width25">크기</div>
-      <div class="ViewRight width75">{{(productDetail.fileSizeBytes/1024/1024).toFixed(1)}}MB</div>
-    </div>
-    <div class="Overview">
-      <div class="ViewLeft width25">카테고리</div>
-      <div class="ViewRight width75">{{productDetail.primaryGenreName}}</div>
-    </div>
-    <div class="Overview">
-      <div class="ViewLeft width25">언어</div>
-      <div class="ViewRight width75">{{productDetail.languageCodesISO2A.join(' | ')}}</div>
-    </div>
-    <div class="Overview">
-      <div class="ViewLeft width25">연령 등급</div>
-      <div class="ViewRight width75">{{productDetail.contentAdvisoryRating}}</div>
-    </div>
-  </div>
-</div>
 </template>
 <script>
 export default {
-  name : "detail",
-  data: function() {
+  name: "detail",
+  data: function () {
     return {
       id: 0,
-      productDetail: null
-    }
+      productDetail: null,
+    };
   },
-  created: function() {
+  created: function () {
     this.id = parseInt(this.$route.params.id);
-    this.$axios.get(`https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=${this.id}&country=KR`)
+    this.$axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=${this.id}&country=KR`
+      )
       .then((response) => {
         this.productDetail = response.data.results[0];
         // console.log(this.productDetail);
@@ -74,211 +99,211 @@ export default {
   },
   methods: {
     iTune(url) {
-      window.open(url, '_blank');
+      window.open(url, "_blank");
     },
     goBack() {
       window.history.back();
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style scoped>
 * {
-    white-space: pre-wrap;
-  }
+  white-space: pre-wrap;
+}
 
 .Container {
-    height: 100%;
-    width: 100%;
-    position: relative;
-    padding: 20px;
-  }
+  height: 100%;
+  width: 100%;
+  position: relative;
+  padding: 20px;
+}
 
-  .BackButton {
-    background: #00000000;
-    color: #0b84fe;
-    text-align: center;
-    border: 0;
-    box-shadow: none;
-    font-size: 18px;
-  }
+.BackButton {
+  background: #00000000;
+  color: #0b84fe;
+  text-align: center;
+  border: 0;
+  box-shadow: none;
+  font-size: 18px;
+}
 
 .Summary {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    margin: 10px;
-  }
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  margin: 10px;
+}
 
 .Icon {
-    width: 100%;
-    height: auto;
-    border-radius: 22.37%;
-  }
+  width: 100%;
+  height: auto;
+  border-radius: 22.37%;
+}
 
-  .TitleName {
-    font-size: 18px;
-    font-weight: 400;
-  }
+.TitleName {
+  font-size: 18px;
+  font-weight: 400;
+}
 
-  .Scope20 {
-    width: 20vw;
-    display: flex;
-    flex-direction: column;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    margin: 0px;
-  }
+.Scope20 {
+  width: 20vw;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  margin: 0px;
+}
 
 .Scope30 {
-    width: 30vw;
-    display: flex;
-    flex-direction: column;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    margin: 0px;
-  }
+  width: 30vw;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  margin: 0px;
+}
 
 .Scope50 {
-    width: 50vw;
-    display: flex;
-    flex-direction: column;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    margin: 5px;
-  }
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  margin: 5px;
+}
 
 .StoreButton {
-    border-radius: 25px;
-    background: #1c1c1e;
-    padding: 10px;
-    color: #0b84fe;
-    text-align: center;
-    border: 0;
-    box-shadow: none;
-  }
+  border-radius: 25px;
+  background: #1c1c1e;
+  padding: 10px;
+  color: #0b84fe;
+  text-align: center;
+  border: 0;
+  box-shadow: none;
+}
 
-  .ScreenShotP {
-    display: flex;
-    justify-content: space-between;
-    flex-direction: column;
-    height: 46vw;
-    flex-wrap: wrap;
-    overflow: auto;
-    margin: 10px;
-  }
+.ScreenShotP {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 46vw;
+  flex-wrap: wrap;
+  overflow: auto;
+  margin: 10px;
+}
 
-  .ScreenShotP::after {
-    content: '';
-    height: 0;
-    border-bottom: 1px #808084 solid;
-    left: 24px;
-    right: 24px;
-    position: absolute;
-  }
+.ScreenShotP::after {
+  content: "";
+  height: 0;
+  border-bottom: 1px #808084 solid;
+  left: 24px;
+  right: 24px;
+  position: absolute;
+}
 
-  .ScreenShot {
-    display: inline;
-    width: auto;
-    height: 100%;
-    margin: 0px 5px;
-  }
+.ScreenShot {
+  display: inline;
+  width: auto;
+  height: 100%;
+  margin: 0px 5px;
+}
 
-  .Explanation {
-    line-height: 1.5;
-    word-break: break-all;
-  }
+.Explanation {
+  line-height: 1.5;
+  word-break: break-all;
+}
 
-  .UpdateviewP::before {
-    content: '';
-    height: 0;
-    border-bottom: 1px #808084 solid;
-    left: 24px;
-    right: 24px;
-    position: absolute;
-  }
+.UpdateviewP::before {
+  content: "";
+  height: 0;
+  border-bottom: 1px #808084 solid;
+  left: 24px;
+  right: 24px;
+  position: absolute;
+}
 
-  .UpdateviewP::after {
-    content: '';
-    height: 0;
-    border-bottom: 1px #808084 solid;
-    left: 24px;
-    right: 24px;
-    position: absolute;
-  }
+.UpdateviewP::after {
+  content: "";
+  height: 0;
+  border-bottom: 1px #808084 solid;
+  left: 24px;
+  right: 24px;
+  position: absolute;
+}
 
-  .UpdateDate {
-    width:100%;
-    line-height: 1.5;
-    display: flex;
-    flex-direction: row;
-    margin: 8px 0px;
-  }
+.UpdateDate {
+  width: 100%;
+  line-height: 1.5;
+  display: flex;
+  flex-direction: row;
+  margin: 8px 0px;
+}
 
-  .UpdateDetail {
-    width:100%;
-    line-height: 1.5;
-    margin: 8px 0px;
-  }
-  
-  .OverviewP {
-    line-height: 1.5;
-  }
+.UpdateDetail {
+  width: 100%;
+  line-height: 1.5;
+  margin: 8px 0px;
+}
 
-  .Overview {
-    width:100%;
-    line-height: 1.5;
-    display: flex;
-    flex-direction: row;
-    margin: 8px 0px;
-  }
+.OverviewP {
+  line-height: 1.5;
+}
 
-  .Overview::after {
-    content: '';
-    height: 0;
-    border-bottom: 1px #808084 solid;
-    left: 24px;
-    right: 24px;
-    position: absolute;
-  }
+.Overview {
+  width: 100%;
+  line-height: 1.5;
+  display: flex;
+  flex-direction: row;
+  margin: 8px 0px;
+}
 
-  .ViewLeft {
-    text-align: left;
-    display: flex;
-    flex-direction: column;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    overflow: hidden;
-  }
+.Overview::after {
+  content: "";
+  height: 0;
+  border-bottom: 1px #808084 solid;
+  left: 24px;
+  right: 24px;
+  position: absolute;
+}
 
-  .ViewRight {
-    text-align: right;
-    display: flex;
-    flex-direction: column;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    overflow: hidden;
-  }
+.ViewLeft {
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  overflow: hidden;
+}
 
-  .width25 {
-    width: 25vw;
-  }
+.ViewRight {
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  overflow: hidden;
+}
 
-  .width50 {
-    width: 50vw;
-  }
+.width25 {
+  width: 25vw;
+}
 
-  .widt75 {
-    width: 75vw;
-  }
+.width50 {
+  width: 50vw;
+}
 
-  .Title {
-    font-size: 20px;
-    font-weight: 800;
-  }
+.widt75 {
+  width: 75vw;
+}
+
+.Title {
+  font-size: 20px;
+  font-weight: 800;
+}
 </style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <h2 v-if="appData && appData.title">{{ appData.title }}</h2>
-    
+
     <div v-if="appData && appData.results">
-      <div class="OuterContainer" v-for="(val, key) in appData.results" v-bind:key="key">
-        <router-link v-bind:to="'/app/'+val.id">
+      <div
+        class="OuterContainer"
+        v-for="(val, key) in appData.results"
+        v-bind:key="key"
+      >
+        <router-link v-bind:to="'/app/' + val.id">
           <div class="InnerContainer">
             <div class="Scope20">
               <a :title="val.name + ' details'">
@@ -13,14 +17,12 @@
             </div>
             <div class="Scope60">
               <span class="Name">{{ val.name }}</span>
-                <span class="Genre" v-if="val.genres[0].name">
-                  {{ val.genres[0].name }}
-                </span>
+              <span class="Genre" v-if="val.genres[0].name">
+                {{ val.genres[0].name }}
+              </span>
             </div>
             <div class="Scope20">
-              <button v-on:click=iTune(val.url)>
-                GET
-              </button>
+              <button v-on:click="iTune(val.url)">GET</button>
             </div>
           </div>
         </router-link>
@@ -30,86 +32,87 @@
 </template>
 <script>
 export default {
-  name: 'products',
-  props: ['appData'],
+  name: "products",
+  props: ["appData"],
   methods: {
     iTune(url) {
-      window.open(url, '_blank');
-    }
-  }
-}
+      window.open(url, "_blank");
+    },
+  },
+};
 </script>
 <style scoped>
 .OuterContainer {
-    width: 100%;
-    background-color: #000000;
-    padding: 0px;
-  }
-  
-  .OuterContainer::after {
-    content: '';
-    height: 0;
-    border-bottom: 1px #808084 solid;
-    left: 24px;
-    right: 24px;
-    position: absolute;
-  }
-  
-  .OuterContainer:last-child::after {
-    border-bottom: 0px #808084 solid;
-  }
-  
-  .InnerContainer {
-    cursor: pointer;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    background-color: #000000;
-    padding: 6px 0px;
-  }
-  
-  .Scope60 {
-    width:60%;
-    display: flex;
-    flex-direction: column;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    padding: 0px 10px;
-  }
-  
-  .Scope20 {
-    width:20%;
-    display: flex;
-    flex-direction: column;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-  }
-  
-  .icon {
-    width: 100%;
-    height: auto;
-    border-radius: 22.37%;
-  }
-  
-  .button, button {
-    border-radius: 25px;
-    background: #1c1c1e;
-    padding: 10px;
-    color: #0b84fe;
-    text-align: center;
-    border: 0;
-    box-shadow: none;
-  }
-  
-  .Name {
-    font-size:14pt;
-    color: #ffffff;
-  }
-  
-  .Genre {
-    font-size:12pt;
-    color: #808084;
-  }
+  width: 100%;
+  background-color: #000000;
+  padding: 0px;
+}
+
+.OuterContainer::after {
+  content: "";
+  height: 0;
+  border-bottom: 1px #808084 solid;
+  left: 24px;
+  right: 24px;
+  position: absolute;
+}
+
+.OuterContainer:last-child::after {
+  border-bottom: 0px #808084 solid;
+}
+
+.InnerContainer {
+  cursor: pointer;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  background-color: #000000;
+  padding: 6px 0px;
+}
+
+.Scope60 {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  padding: 0px 10px;
+}
+
+.Scope20 {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+}
+
+.icon {
+  width: 100%;
+  height: auto;
+  border-radius: 22.37%;
+}
+
+.button,
+button {
+  border-radius: 25px;
+  background: #1c1c1e;
+  padding: 10px;
+  color: #0b84fe;
+  text-align: center;
+  border: 0;
+  box-shadow: none;
+}
+
+.Name {
+  font-size: 14pt;
+  color: #ffffff;
+}
+
+.Genre {
+  font-size: 12pt;
+  color: #808084;
+}
 </style>
